51e920b5e7db0f2ae220003cb38cc99e9ad27a7b
AW-Project <114434313+AW-Project@users.noreply.github.com>
2022-10-01 17:16:03 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Chat Box - AWProject</title>
+		<link rel="stylesheet" href="assets/css/style.css">
+		<script type="text/javascript" src="assets/js/jquery-1.9.0.min.js"></script>
+		<script type="text/javascript" src="assets/js/script.js"></script>
+	</head>
+	<body>
+		<div class="shout_box">
+			<div class="header">
+				AWProject Message Box
+				<div class="close_btn">&nbsp;</div>
+			</div>
+  			<div class="toggle_chat">
+  				<div class="message_box"></div>
+    			<div class="user_info">
+    				<input name="shout_username" id="shout_username" type="text" placeholder="Your Name" maxlength="15" />
+   					<input name="shout_message" id="shout_message" type="text" placeholder="Type Message Hit Enter" maxlength="100" /> 
+    			</div>
+    		</div>
+		</div>
+	</body>
+</html>
